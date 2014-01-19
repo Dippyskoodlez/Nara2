a8f81e6e3eb3793475590d9a9b7d8080bbd7be23
Dippyskoodlez <sabilleg@gmail.com>
2014-01-19 16:00:16 -0600

    Update Nara 2.ino
    Complete overhaul from old nara.

--- Nara 2.ino
+++ Nara 2.ino
@@ -1,162 +1,138 @@
-
-  //-------------------------------------------------------// 
-  #include <AFMotor.h> //Adafruit motor header library: http://www.adafruit.com/products/81
-
-//Motor Layout (Wiring specific)
-  AF_DCMotor motorFL(4, MOTOR12_64KHZ);
-  AF_DCMotor motorBL(1, MOTOR12_64KHZ); 
-  AF_DCMotor motorFR(2, MOTOR12_64KHZ);  
-  AF_DCMotor motorBR(3, MOTOR12_64KHZ);
-
-  char rcvDirection = 0; // Forward (F), Backward(B), Neutral steer(N)
-  int rcvSpeedL = 0; //0-128?
-  int rcvSpeedR = 0;
-  bool validCmd = False;
- 
- //-------------------------------------------------------// 
-void  setup(){
-  //Starts the serial connection
-  Serial.begin(9600);
-  Serial.write("Hello Dave.");
-}
-//-------------------------------------------------------// 
-void loop(){ 
-  //Reads the incoming command.
-  ReadIncoming();
-  //Executes the command. Fetch boy, fetch!
-  ExecuteCommands();
-
-}
-//-------------------------------------------------------// 
-void ReadIncoming(){
-  if (Serial.available()) {
-    
-  }
-  while(validCmd == false) {
-  //Read the incoming command, 1 byte, into memory to use as a command.
- rcvDirection = Serial.read();
- rcvSpeedL = Serial.read();
- rcvSpeedR = Serial.read();
-
- if (rcvDirection == 'F' || 'B' || 'n'){
-  if (rcvSpeedL >= 0 && rcvSpeedL <= 128){
-    if rcvSpeedR >= 0 && rcvSpeedR <= 128) {
-        validCmd = True ;
-       }
-     }
-    }
-  }
-
-}
-//-------------------------------------------------------// 
-void ExecuteCommands(){
+ //-------------------------------------------------------//  
+  #include <AFMotor.h> //Adafruit motor header library: http://www.adafruit.com/products/81 
   
-  //Translate the command into a direction. F/B: Fwd/reverse, L/R: Left, Right, l/r: reverse turn.
-  switch(rcvDirection){
-    //Checks the first byte recieved for the direction to start: Forward, Backward or Neutral steer.
-    case 'F':
-    // Reply with the direction intended to go
-      Serial.println("GOING STRAIGHT AT:");
-      Serial.println(rcvSpeedL, rcvspeedR);
-      //Start moving with forward intent.
-      GoForward(rcvSpeedL, rcvSpeedR);
-      break;
-    case 'B':
-    //Reply with the direction to go
-      Serial.println("GOING BACKWARDS AT:");
-      Serial.println(rcvSpeedL, rcvSpeedR);
-      //Start moving with backwards intent.
-      GoBackwards(rcvSpeedL, rcvSpeedR);
-      break;
-    case 'N':
-    // Dat neutral steer.
-      Serial.println("NEUTRAL JUST KICKED IN YO")
-      NeutralSteer(rcvSpeedL, rcvSpeedR);
-      break;
-  }
-}
-//-------------------------------------------------------// 
-//Set motor driver to appropriate fwd speeds.
-void GoForward(int FSpeed){
-  //seperates stop and go command.  Min/Max motor speeds here.
-  if(FSpeed > 0 && FSpeed < 10){
-    motorL.setSpeed(FSpeed*20);
-    motorR.setSpeed(FSpeed*20);
-    motorL.run(FORWARD);
-    motorR.run(FORWARD);
-  }
-  else if(FSpeed == 0){
-    //STOP.
-    motorL.setSpeed(0);
-    motorR.setSpeed(0);
-  }
-}
-//-------------------------------------------------------// 
-void GoBackwards(int FSpeedB){
-  //Same as fwd, except reverse.
-  if(FSpeedB > 0 && FSpeedB < 10){
-    motorL.setSpeed(FSpeedB*20);
-    motorR.setSpeed(FSpeedB*20);
-    motorL.run(BACKWARD);
-    motorR.run(BACKWARD);
-  }
-  else if(FSpeedB == 0){
-    //STOP.
-    motorL.setSpeed(0);
-    motorR.setSpeed(0);
-  }
-}
-//-------------------------------------------------------// 
-void TurnLeftF(int FSpeedM,int FSpeedB){
-  //Left turn by reducing the left motor speed
-  if (FSpeedB < 10 && FSpeedB > 0 && FSpeedM < 10 && FSpeedM > 0){
-    motorL.setSpeed((FSpeedB*(20-(FSpeedM*2) )));
-    motorR.setSpeed(FSpeedB*20);
-    motorL.run(FORWARD);
-    motorR.run(FORWARD);
-  }
-  //Neutral turn
-  else if(FSpeedB == 0 && FSpeedM > 0 && FSpeedM < 10){
-    motorL.setSpeed(FSpeedM*20);
-    motorR.setSpeed(FSpeedM*20);
-    motorL.run(BACKWARD);;
-    motorR.run(FORWARD);
-  }
-}
-//-------------------------------------------------------// 
-void TurnRightF(int FSpeedM,int FSpeedB){
-  //Right turn by reducing Right motors.
-  if (FSpeedB < 10 && FSpeedB > 0 && FSpeedM < 10 && FSpeedM > 0){
-    motorR.setSpeed((FSpeedB*(20-(FSpeedM*2) )));
-    motorL.setSpeed(FSpeedB*20);
-    motorL.run(FORWARD);
-    motorR.run(FORWARD);
-  }
-  //Neutral steer right
-  else if(FSpeedB == 0 && FSpeedM > 0 && FSpeedM < 10){
-    motorL.setSpeed(FSpeedM*20);
-    motorR.setSpeed(FSpeedM*20);
-    motorR.run(BACKWARD);;
-    motorL.run(FORWARD);
-  }
-}
-//-------------------------------------------------------// 
-void TurnLeftB(int FSpeedM,int FSpeedB){
-  //Turns left while in reverse
-  if (FSpeedB < 10 && FSpeedB > 0 && FSpeedM < 10 && FSpeedM > 0){
-    motorL.setSpeed((FSpeedB*(20-(FSpeedM*2) )));
-    motorR.setSpeed(FSpeedB*20);
-    motorL.run(BACKWARD);
-    motorR.run(BACKWARD);
-  }
-}
-//-------------------------------------------------------// 
-void TurnRightB(int FSpeedM,int FSpeedB){
-  //Turn right while in reverse
-  if (FSpeedB < 10 && FSpeedB > 0 && FSpeedM < 10 && FSpeedM > 0){
-    motorR.setSpeed((FSpeedB*(20-(FSpeedM*2) )));
-    motorL.setSpeed(FSpeedB*20);
-    motorL.run(BACKWARD);
-    motorR.run(BACKWARD);
-  }
+//Motor Layout (Wiring specific) 
+  AF_DCMotor motorFL(4, MOTOR12_64KHZ); 
+  AF_DCMotor motorBL(1, MOTOR12_64KHZ);  
+  AF_DCMotor motorFR(2, MOTOR12_64KHZ);   
+  AF_DCMotor motorBR(3, MOTOR12_64KHZ); 
+  
+  char rcvDirection = 0; // Forward (F), Backward(B), Neutral steer(N) 
+  int rcvSpeedL = 0; //0-255 
+  int rcvSpeedR = 0; 
+  bool validCmd = False; 
+   
+ //-------------------------------------------------------//  
+void  setup(){ 
+  //Starts the serial connection 
+  Serial.begin(9600); 
+  Serial.write("Hello Dave."); 
+  //Turn on motors. (released state) 
+  motorFL.run(RELEASE); 
+  motorBL.run(RELEASE); 
+  motorFR.run(RELEASE); 
+  motorBR.run(RELEASE); 
+} 
+//-------------------------------------------------------//  
+void loop(){  
+  //Reads the incoming command. 
+  ReadIncoming(); 
+  //Executes the command. Fetch boy, fetch! 
+  ExecuteCommands(); 
+  
+} 
+  
+/*-------------------------------------- 
+  Process the proper axis for sending motor speeds, based on quadrant read. 
+  These are the Values that need SENT for proper motion, over the READ Values. 
+        (F)    (Y)   (F) 
+              (255) 
+        IV      |     I 
+    X:R Y:L     |  X:L Y:R 
+    X:-R  Y:L   |  X:L Y:R 
+(-X)(-255)-----------------(255-)(X) 
+        (B)     |    (B) 
+         III    |     II 
+     X:-R Y:-L  | X:-L Y:-R 
+     X:-R Y:-L  | X:L  Y:-R 
+             (-255) 
+              (-Y) 
+----------------------------------------*/
+  
+//-------------------------------------------------------//  
+void ReadIncoming(){ 
+  if (Serial.available()) { 
+      
+  } 
+  while(validCmd == false) { 
+  //Read the incoming command, 1 byte, into memory to use as a command. 
+ rcvDirection = Serial.read(); 
+ rcvSpeedL = Serial.read(); 
+ rcvSpeedR = Serial.read(); 
+  
+//Verify valid direction and min/max speed recieved. 
+ if (rcvDirection == 'F' || 'B' || 'n'){ 
+  if (rcvSpeedL >= 0 && rcvSpeedL <= 255){ 
+    if rcvSpeedR >= 0 && rcvSpeedR <= 255) { 
+        validCmd = True ; 
+       } 
+     } 
+    } 
+  } 
+  
+} 
+//-------------------------------------------------------//  
+void ExecuteCommands(){ 
+    
+  //Translate the command into a direction. F/B: Fwd/reverse, L/R: Left, Right, l/r: reverse turn. 
+  switch(rcvDirection){ 
+    //Checks the first byte recieved for the direction to start: Forward, Backward or Neutral steer. 
+    case 'F': 
+    // Reply with the direction intended to go 
+      Serial.println("GOING STRAIGHT AT:"); 
+      Serial.println(rcvSpeedL, rcvspeedR); 
+      //Start moving with forward intent. 
+      GoForward(rcvSpeedL, rcvSpeedR); 
+      break; 
+    case 'B': 
+    //Reply with the direction to go 
+      Serial.println("GOING BACKWARDS AT:"); 
+      Serial.println(rcvSpeedL, rcvSpeedR); 
+      //Start moving with backwards intent. 
+      GoBackwards(rcvSpeedL, rcvSpeedR); 
+      break; 
+    case 'N': 
+    // Dat neutral steer. 
+      Serial.println("NEUTRAL JUST KICKED IN YO") 
+      NeutralSteer(rcvSpeedL, rcvSpeedR); 
+      break; 
+  } 
+} 
+//-------------------------------------------------------//  
+//Set motor driver to appropriate fwd speeds. 
+void GoForward(int FSpeedL, int FSpeedR){ 
+  //seperates stop and go command.  Min/Max motor speeds here. 
+    
+    motorFL.setSpeed(FSpeedL); 
+    motorBR.setSpeed(FSpeedL); 
+    motorFR.setSpeed(FSpeedR); 
+    motorBR.setSpeed(FSpeedR); 
+    motorFL.run(FORWARD); 
+    motorFR.run(FORWARD); 
+    motorFR.run(FORWARD); 
+    motorBR.run(FORWARD); 
+} 
+//-------------------------------------------------------//  
+void GoBackwards(int FSpeedL, int FSpeedR){ 
+  //Same as fwd, except reverse. 
+    motorFL.setSpeed(FSpeedL); 
+    motorBR.setSpeed(FSpeedL); 
+    motorFR.setSpeed(FSpeedR); 
+    motorBR.setSpeed(FSpeedR); 
+    motorFL.run(BACKWARD); 
+    motorFR.run(BACKWARD); 
+    motorFR.run(BACKWARD); 
+    motorBR.run(BACKWARD); 
+} 
+//-------------------------------------------------------//  
+void NeutralSteer(int FSpeedL, int FSpeedR){ 
+  
+//Neutral Left 
+  //invert numbers... -255? 
+if (FSpeedL <= -10 && FSpeedL >= 0) 
+  
+  
+//Neutral right 
+  
+  
 }
